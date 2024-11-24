24d89c069270e42e7ecdd465c21825682c5f7cf1
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-24 12:04:14 +0530

    Finisth the GSAP Part

--- Projects/02-Text-Raval-Animation/script.js
+++ Projects/02-Text-Raval-Animation/script.js
@@ -6,5 +6,25 @@ charecters.forEach((char => {
     console.log(char);
     let span = document.createElement("span");
     span.textContent = char == " " ? "\u00A0" : char;
+    span.className ="char";
     document.querySelector(".container").appendChild(span);
 }));
+
+let tl = gsap.timeline();
+
+tl.from(".char" , {
+    opacity:0,
+    y:50,
+    stagger:0.05,
+    duration: 0.5,
+    ease:"back.out(1.5)",
+}).to(".char",  {
+    y:-20,
+    stagger:{
+        each:0.05,
+        repeat:1,
+        yoyo:true,
+    },
+    ease:"power1.inOut",
+    duration:0.3,
+})
