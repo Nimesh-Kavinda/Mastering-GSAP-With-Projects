bec4846435503fa79457723fa0e3f65065db5e55
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-28 20:33:34 +0530

    Start to learn some GSAP propertice

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -3,4 +3,4 @@ const image1 = document.querySelector('.img1')
 document.querySelector('.btn')
 .addEventListener("click", () => {
 gsap.fromTo(image1, {autoAlpha: 0}, {autoAlpha: 1, duration : 2}) 
-})
+})   
