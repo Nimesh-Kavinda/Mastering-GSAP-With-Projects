af05d771bd19dc97a2a0d393f72ca7929f252a20
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-28 20:49:46 +0530

    Learned some Utility Properties

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,3 +1,13 @@
-const image1 = document.querySelector('.img1')
+const image1 = document.querySelector('.img1');
 
-gsap.from(image1, {autoAlpha: 0, y: -100, rotation: 90, ease: "bounce", duration : 2}) 
+gsap.from(image1, {
+  autoAlpha: 0,
+  y: -100,
+  rotation: 90,
+  ease: 'bounce',
+  duration: 2,
+  delay: 1,
+  repeat: -1,
+  repeatDelay: 0.2,
+  yoyo: true,
+});
