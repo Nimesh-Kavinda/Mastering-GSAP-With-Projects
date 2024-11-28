ad01116825f48e26c5424e52e1ed4fd1d90967c7
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-28 19:24:14 +0530

    finish "gasp.from" method

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,7 +1,4 @@
 const image1 = document.querySelector('.img1')
 
-document.querySelector('.btn')
-.addEventListener("click", () => {
-    gsap.from(image1, {autoAlpha: 0, duration : 2}) 
-})
+gsap.from(image1, {autoAlpha: 0, rotation : 45, scale:0.5, x: 100, y:-100, duration : 2}) 
 
