1a391c9dab20730300dd30e4dddab4e605e84536
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-28 21:19:19 +0530

    using Animation random values in a range

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,12 +1,7 @@
 const image1 = document.querySelector('.img1');
 
-gsap.from('img', {
-  autoAlpha: 0,
-  y: -100,
-  ease: 'power4',
-  duration: 2,
-  stagger: {
-    each: 0.5,
-    from: 'end',
-  },
+gsap.to('.btn', {
+  duration: 0.4,
+  stagger: 0.2,
+  x: 'random(-100,100)',
 });
