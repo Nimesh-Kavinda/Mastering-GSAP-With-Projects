83b2bd930cdee1f788b6d8a87cbfde2f1786aeb9
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-28 19:01:27 +0530

    Finish "gsap.to" method

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,3 +1,3 @@
 const image1 = document.querySelector('.img1')
 
-gsap.to(image1, {x: 100, y: 100, backgroundColor: "red", duration: 2})
+gsap.to(image1, {x: 100, y: 100, backgroundColor: "red", duration: 2})  
