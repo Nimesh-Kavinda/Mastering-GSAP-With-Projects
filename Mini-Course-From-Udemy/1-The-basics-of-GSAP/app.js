a612525091c6c6b17e2244d9f38738d8086147d3
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-29 22:17:47 +0530

    learned some utility methods in tween

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,3 +1,13 @@
 const image1 = document.querySelector('.img1');
 
-gsap.set(image1, { opacity: 0 });
+const tween = gsap.to('.img2', { y: 200 });
+
+// tween.kill();
+// tween.delay(1);
+// tween.duration(5);
+
+setTimeout(() => {
+  // tween.resume();
+  tween.seek(2.5);
+  tween.play();
+}, 2000);
