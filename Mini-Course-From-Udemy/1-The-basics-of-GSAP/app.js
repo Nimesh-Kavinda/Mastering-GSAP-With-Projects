17b1cc8a50bef2763654de21b9acd2b8a11233d7
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-29 22:11:06 +0530

    learned about gsap.set Method

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,17 +1,3 @@
 const image1 = document.querySelector('.img1');
 
-gsap.registerEffect({
-  name: 'imgAnimation',
-  effect: (targets, config) => {
-    return gsap.to(targets, {
-      duration: config.duration,
-      y: 200,
-      scale: 1.4,
-      rotation: 360,
-    });
-  },
-  defaults: { duration: 2 },
-});
-
-gsap.effects.imgAnimation(image1, { duration: 5 });
-gsap.effects.imgAnimation('.img2');
+gsap.set(image1, { opacity: 0 });
