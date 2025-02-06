a8e8d83babe138cd7ce467ced3487207c1b7d5a6
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-06 16:46:14 +0530

    learn posotion parameeters

--- Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
+++ Mini-Course-From-Udemy/1-The-basics-of-GSAP/app.js
@@ -1,6 +1,8 @@
 const tl = gsap.timeline();
 
-tl.from('.img1', {autoAlpha: 0, duration: 1, y: -50})
-tl.from('.img2', {autoAlpha: 0, duration: 1, y: -50})
-tl.from('.img3', {autoAlpha: 0, duration: 1, y: -50})
-tl.from('h1', {autoAlpha: 0, duration: 1, y: -50})
+tl
+.from('.img1', {autoAlpha: 0, duration: 1, y: -50})
+.from('.img2', {autoAlpha: 0, duration: 1, y: -50}, '<') 
+.from('.img3', {autoAlpha: 0, duration: 1, y: -50},'-=0.75')
+.from('h1', {autoAlpha: 0, duration: 1, y: -50},'-=0.75')
+.from('p', {autoAlpha: 0, duration: 1, y: -50},'-=0.75')
